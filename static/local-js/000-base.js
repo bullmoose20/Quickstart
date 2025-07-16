f4b4254717cd28e26c8606cf8da4d71eed0cb592
bullmoose20 <12549033+bullmoose20@users.noreply.github.com>
2025-07-16 07:27:22 -0400

    [44] removed word wrap in logs showing in UI (#742)

--- static/local-js/000-base.js
+++ static/local-js/000-base.js
@@ -204,7 +204,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (data.success) {
           resultBox.innerHTML = `
             <strong>✅ Update Successful!</strong><br>
-            <pre class="form-control bg-dark text-light" style="height: 300px; overflow-y: scroll; white-space: pre-wrap;">
+            <pre class="form-control bg-dark text-light" style="height: 300px; overflow-y: auto; overflow-x: auto; white-space: pre;">
               ${data.git_output}${data.pip_output}
             </pre>
             <button class="btn btn-sm btn-success mt-2" onclick="restartQuickstart()">Restart Quickstart</button>
